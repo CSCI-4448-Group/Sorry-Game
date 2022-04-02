cbb55893ccd71cab503321c71fec21c0a3192970
brno4758 <77458854+brno4758@users.noreply.github.com>
2022-04-01 22:51:54 -0600

    Update Pawn.java

--- src/main/java/com/project/sorryapp/Pawn.java
+++ src/main/java/com/project/sorryapp/Pawn.java
@@ -5,4 +5,12 @@ import javafx.scene.shape.Circle;
 public class Pawn extends Circle {
     GameColor color_;
     Tile currTile_;
+
+    Pawn(GameColor color){
+        color_ = color;
+    }
+    Pawn(GameColor color, Tile startTile){
+        color_ = color;
+        currTile_ = startTile;
+    }
 }

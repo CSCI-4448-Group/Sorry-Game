504530e12adf658f93f4a4fa47a39404df7abf3d
Brian <brno4758@colorado.edu>
2022-04-04 21:21:25 -0600

    Removed duplicate files

--- src/main/java/com/project/sorryapp/cards/OneCard.java
+++
@@ -1,9 +0,0 @@
-package com.project.sorryapp.cards;
-
-public class OneCard extends Card {
-    public OneCard()
-    {
-        set_card_value(1);
-        set_description("Start a pawn out or moves pawn forward 1 space.");
-    }
-}

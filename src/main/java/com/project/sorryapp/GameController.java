e8619bff21eb716cb7da3b8776e08d30dee47440
Sidhant Puntambekar <sidhantnp@yahoo.com>
2022-04-06 20:45:37 -0600

    Updates with image

--- src/main/java/com/project/sorryapp/GameController.java
+++ src/main/java/com/project/sorryapp/GameController.java
@@ -3,10 +3,13 @@ package com.project.sorryapp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 
 public class GameController implements SceneLoader {
@@ -44,7 +47,7 @@ public class GameController implements SceneLoader {
     }
 
     @FXML
-    public void on_home_clicked(ActionEvent event){
+    public void on_home_clicked(ActionEvent event) {
         Node node = (Node)event.getSource();
         Stage thisStage = (Stage)node.getScene().getWindow();
         load_scene("home-view.fxml", thisStage);
--- src/main/java/com/project/sorryapp/HomeController.java
+++ src/main/java/com/project/sorryapp/HomeController.java
@@ -7,8 +7,12 @@ import javafx.scene.control.Label;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
 
 //repeated code here, figure this out (i.e., this would get ugly if we had more than 2 buttons)
 public class HomeController implements SceneLoader{
@@ -27,9 +31,12 @@ public class HomeController implements SceneLoader{
     }
 
     @FXML
-    public void on_newgame_clicked(ActionEvent event){
+    public void on_newgame_clicked(ActionEvent event) throws Exception {
         Node node = (Node)event.getSource();
         Stage thisStage = (Stage)node.getScene().getWindow();
+//        FileInputStream input = new FileInputStream("");
+//        Image image = new Image(input);
+//        ImageView imageView = new ImageView(image);
         load_scene("game-view.fxml", thisStage);
     }
 }

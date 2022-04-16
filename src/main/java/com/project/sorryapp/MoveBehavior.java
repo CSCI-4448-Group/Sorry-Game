b3c024d2ca34d40838ffb1f35b672e746b7cd741
brno4758 <brno4758@colorado.edu>
2022-04-15 22:29:59 -0600

    Simplify if else if blocks, add missing check_kick logic

--- src/main/java/com/project/sorryapp/MoveBehavior.java
+++ src/main/java/com/project/sorryapp/MoveBehavior.java
@@ -27,10 +27,12 @@ public interface MoveBehavior{
 class NormalMove implements MoveBehavior{
     @Override
     public boolean move_pawn(Pawn pawn, int distance) {
-        if (distance >= 0)
+        if(distance == 0){
+            check_kick(pawn);
+            return true;
+        }
+        else if (distance > 0)
         {
-            if(distance == 0) //Still need to check if we kick a pawn, if we land on a slide, etc.
-                return true;
             Tile nextTile = pawn.get_tile().get_next(); //Get the next and current tile
             Tile currTile = pawn.get_tile();
             nextTile.add_pawn(pawn); //Set the tiles pawn
@@ -40,17 +42,12 @@ class NormalMove implements MoveBehavior{
         }
         else
         {
-            if(distance == 0) //Still need to check if we kick a pawn, if we land on a slide, etc.
-                return true;
             Tile prevTile = pawn.get_tile().get_prev(); //Get the next and current tile
             Tile currTile = pawn.get_tile();
-
-            if (prevTile == null)
-            {
+            if (prevTile == null) {
                 System.out.println("Four or Ten Card Pulled. Backwards move from home tile not possible.");
                 return true;
             }
-
             prevTile.add_pawn(pawn); //Set the tiles pawn
             pawn.set_tile(prevTile); //Set the pawns tile
             currTile.remove_pawn(pawn);
@@ -62,12 +59,12 @@ class NormalMove implements MoveBehavior{
 class GatewayMove implements MoveBehavior{
     @Override
     public boolean move_pawn(Pawn pawn, int distance) {
-        if (distance >= 0)
+        if(distance == 0){
+            check_kick(pawn);
+            return true;
+        }
+        else if(distance >0)
         {
-            if(distance == 0) {
-                check_kick(pawn);
-                return true;
-            }
             if(pawn.getFill().equals(pawn.get_tile().getFill())){ //If the pawns color matches the gateways color
                 Tile nextTile = ((GatewayTile)pawn.get_tile()).get_gateway_next();
                 Tile currTile = pawn.get_tile();
@@ -78,15 +75,10 @@ class GatewayMove implements MoveBehavior{
             }
             else{ //This is kind of cheese, but if the pawn doesnt go into the gated safezone, just let a normal move behavior handle the move
                 MoveBehavior normalMove = new NormalMove();
-                normalMove.move_pawn(pawn, distance);
-                return false;
+                return normalMove.move_pawn(pawn, distance);
             }
         }
         else {
-            if (distance == 0) {
-                check_kick(pawn);
-                return true;
-            }
             if (pawn.getFill().equals(pawn.get_tile().getFill())) { //If the pawns color matches the gateways color
                 Tile nextTile = ((GatewayTile) pawn.get_tile()).get_gateway_next();
                 Tile currTile = pawn.get_tile();
@@ -96,8 +88,7 @@ class GatewayMove implements MoveBehavior{
                 return nextTile.perform_move(pawn, distance + 1);
             } else { //This is kind of cheese, but if the pawn doesnt go into the gated safezone, just let a normal move behavior handle the move
                 MoveBehavior normalMove = new NormalMove();
-                normalMove.move_pawn(pawn, distance);
-                return false;
+                return normalMove.move_pawn(pawn, distance);
             }
         }
     }
